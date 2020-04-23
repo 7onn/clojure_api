0a4546b5ee373bed01ce548daf675380fb7aac2f
tom <devbytom@gmail.com>
2020-04-23 19:56:20 -0300

    testing webhook for PR (#1)
    adding handlers for pr push and release

---
+++ src/clojure_api/webhook.clj
@@ -0,0 +1,102 @@
+(ns clojure_api.webhook
+  (:require 
+    [org.httpkit.server :as server]
+    [clj-time.core :as t]
+    [compojure.core :refer :all]
+    [compojure.route :as route]
+    [clojure.pprint :as pp]
+    [clojure.string :as str]
+    [clojure.data.json :as json]
+    [ring.middleware.defaults :refer :all]
+    [ring.middleware.json :refer [wrap-json-response wrap-json-body]]
+    [ring.util.response :refer [response]]
+  )
+)
+
+(defn default-res-headers
+  []
+  { 
+    "content-type" "application/json"
+    "when" (str (java.util.Date.))
+  }
+)
+
+(defn gh-release
+  [body]
+  (def commit (get body "after"))
+  (def ref (get body "ref"))
+  (def sender (get (get body "sender") "login"))
+  
+  (def responseBody (json/write-str (str "{\"function\": \"gh-release\", \"sender\": \"" sender "\", \"ref\": \"" ref "\" \"commit\": \"" commit "\"}")))
+  (println responseBody)
+  {
+    :status 200
+    :body responseBody
+    :headers (default-res-headers)
+  }
+)
+
+(defn gh-master
+  [body]
+  (def commit (get body "after"))
+  (def sender (get (get body "sender") "login"))
+
+  (def responseBody (json/write-str (str "{\"function\": \"gh-master\", \"sender\": \"" sender "\", \"commit\": \"" commit "\"}")))
+  (println responseBody)
+  {
+    :status 200
+    :body responseBody
+    :headers (default-res-headers)
+  }
+)
+(defn gh-pull-request
+  [body]
+  (def commit (get (get (get body "pull_request") "head") "sha"))
+  (def prnumber (get body "number"))
+  (def sender (get (get body "sender") "login"))
+  
+  (def responseBody (json/write-str (str "{\"function\": \"gh-pull-request\", \"sender\": \"" sender "\", \"prnumber\": \"" prnumber "\", \"commit\": \"" commit "\"}")))
+  (println responseBody)
+
+  {
+    :status 200
+    :headers (default-res-headers)
+    :body responseBody
+  }
+)
+
+(defn webhook
+  [req]
+  (def body (json/read-str (slurp (:body req))))
+
+  (def ref (get body "ref"))
+  (def pull_request (get body "pull_request"))
+
+  (cond
+    (and ref (re-matches #"^refs/tags/v\d{1,2}.\d{1,2}.\d{1,2}$" ref)) (gh-release body)
+    (and ref (re-matches #"^refs/heads/master$" ref)) (gh-master body)
+    (some? pull_request) (gh-pull-request body)
+    :else 
+      {
+        :status 404
+        :headers (default-res-headers)
+      }
+  )
+)
+
+(defroutes app-routes
+  (GET "/" [] {:status 200 :body "ok"})
+  (POST "/webhook" [] webhook)
+  (route/not-found "Error, page not found!"))
+
+(def app
+  (wrap-defaults app-routes api-defaults)
+)
+
+(defn -main
+  [& args]
+  (let [port (Integer/parseInt (or (System/getenv "PORT") "3000"))]
+    (server/run-server app {:port port})
+    (println (str "Running webserver at http:/127.0.0.1:" port "/"))
+  )
+)
